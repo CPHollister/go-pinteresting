5f033071bac1d07122aee38301842bf8db88d04d
Chris Hollister <cphollister@gmail.com>
2013-12-16 10:02:39 -0500

    Your Message

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
 gem 'rails', '4.0.2'
 gem 'sass-rails', '~> 4.0.0'
@@ -8,6 +9,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem 'bootstrap-sass', '~> 3.0.3.0'
+gem 'devise', '~> 3.2.2'
 
 group :development, :test do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+# Required for Heroku. 
+# Note to set this to actual host.
+config.action_mailer.default_url_options = { :host => 'http://go-pinteresting.herokuapp.com/' }
 end
