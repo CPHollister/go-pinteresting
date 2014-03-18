8678b17ec7f180c01867c49246fe279615c5b37c
Chris Hollister <cphollister@gmail.com>
2014-03-18 12:08:16 -0400

    Added Names to Users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,8 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+
+	validates :image, presence: true
+	validates :description, presence: true
+
 end
